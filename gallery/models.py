1ef330be7694ae9a4787852a8fe6850a2270d48b
kennedybabu <kennedynjogu8@gmail.com>
2021-11-26 19:59:19 +0300

    feature: create tests for the User class

--- gallery/models.py
+++ gallery/models.py
@@ -6,3 +6,23 @@ class User(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    def save_user(self):
+        self.save()
+
+class category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    # image =
+    image_name = models.CharField(max_length=50)
+    image_description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    category = models.ManyToManyField(category)
+    pub_date = models.DateTimeField(auto_now_add=True)
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import User, Post, category
 
 # Create your tests here.
+
+class UserTest(TestCase):
+    #set up method
+    def setUp(self):
+        self.jude = User(first_name='jude', last_name='babu', email='jude@moringa.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.jude, User))
+
+    def test_save_method(self):
+        self.jude.save_user()
+        users = User.objects.all()
+        self.assertTrue(len(users) > 0)
